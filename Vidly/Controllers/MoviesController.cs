a274babbbd5b741cbc8568c3395e443a701d4d2b
Ashish Sahu <ashishsahu10428@gmail.com>
2022-05-18 21:41:08 +0530

    Action Results - Datatype of an Action method - There are many Action results wiht different helper functions like ViewResult:View(), ContentResult:Content() and more - Define the type of object that is to be returned after accessing a link

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,7 +9,11 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             Movie m = new Movie() { Title = "Avenger 1" };
-            return View(m);
+            //return View(m);
+            //return Content("Hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
