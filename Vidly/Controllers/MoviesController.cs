1dae1ad3767eea10b8dbda3357654e964319add6
Ashish Sahu <ashishsahu10428@gmail.com>
2022-05-19 00:25:50 +0530

    Action Parameters - Single parameter - Multiple Parameters: have to use query string

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,11 +9,31 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             Movie m = new Movie() { Title = "Avenger 1" };
-            //return View(m);
+            return View(m);
             //return Content("Hello world");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+
+        //Action Parameters Demo
+        public ActionResult Edit(int id)
+        {
+            return Content("ID = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 111;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name2";
+
+            return Content(string.Format(pageIndex + " " + sortBy));
         }
     }
+
+
 }
